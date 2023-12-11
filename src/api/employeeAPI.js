b33c8f8ba7942f1c59af47856d38943d0a1dff9c
Imamul Islam nAyeem <imamul.nayeem@gmail.com>
2023-12-11 14:34:41 +0600

    static employee list added

--- src/api/employeeAPI.js
+++ src/api/employeeAPI.js
@@ -1,5 +1,10 @@
 import axios from 'axios'
-
+const employees = [
+  { id: 1, employeeName: 'John Doe', department: 'HR' },
+  { id: 2, employeeName: 'Jane Smith', department: 'IT' },
+  { id: 3, employeeName: 'Bob Johnson', department: 'Finance' },
+  // Add more employees as needed
+];
 const api=axios.create({
   baseURL: import.meta.env.VITE_API_BASE +'/Employees'
   })
@@ -11,7 +16,8 @@ export function fetchEmployees(){
               // }
             }).then((function (response) {
               // console.log(response.data)
-              return response.data
+              // return response.data
+              return employees;
              
             })).catch(function (error) {
               console.log(error)
